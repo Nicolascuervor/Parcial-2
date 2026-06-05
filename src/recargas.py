40ad3bc5d4749ebbd44489115a12a83b2feb3112
Nicolás <ncuervo_175@cue.edu.co>
2026-06-05 18:18:03 -0500

    Evidencias de imagenes

--- src/recargas.py
+++ src/recargas.py
@@ -1,20 +1,20 @@
 class MontoInvalidoError(Exception):
     pass
 
-
 def procesar_recarga(monto: int, tipo_plan: str) -> int:
-
+    # 1. Validar límites de monto
     if monto < 1000 or monto > 50000:
         raise MontoInvalidoError("El monto de recarga debe estar entre $1.000 y $50.000")
-
+        
+    # 2. Calcular bono base
     bono = 0
     if monto >= 30000:
         bono = 25
     elif monto >= 10000:
         bono = 10
-
+        
     # 3. Aplicar beneficio premium
     if tipo_plan.lower() == "premium":
         bono += 5
-
+        
     return bono
--- tests/performance/locustfile.py
+++ tests/performance/locustfile.py
@@ -3,15 +3,11 @@ import random
 
 
 class ClienteRecargas(HttpUser):
-
     wait_time = between(1, 2)
-
     @task
     def realizar_recargas(self):
-
         monto_random = random.randint(1000, 50000)
         plan = random.choice(["estandar", "premium"])
-
         self.client.get(f"/calcular-recarga?monto={monto_random}&plan={plan}")
 
 @events.quitting.add_listener
@@ -20,11 +16,11 @@ def verificar_rendimiento_p95(environment, **kwargs):
         return
 
     p95 = environment.stats.total.get_response_time_percentile(0.95)
-    print(f"\n--- RESULTADOS DEL ESTRÉS ---")
-    print(f"Tiempo de respuesta P95: {p95} ms")
+    print(f"\nResultado de la prueba de estres")
+    print(f"Tiempo de respuesta: {p95} ms")
 
     if p95 > 300:
-        print("FALLO: El tiempo P95 supera el límite estricto de 300ms.")
+        print("FALLO: El tiempo supera el límite estricto de 300ms.")
         environment.process_exit_code = 1
     else:
         print("ÉXITO: El sistema cumple con la métrica de rendimiento (P95 < 300ms).")
